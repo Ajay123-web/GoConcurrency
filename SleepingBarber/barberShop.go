e0906f014f1d6a6c4e0c2f4dbd5dcaad5ae38afb
Ajay Gupta <ajaykumargupta35304@gmail.com>
2022-08-30 16:46:42 +0530

    shop running

--- SleepingBarber/barberShop.go
+++ SleepingBarber/barberShop.go
@@ -15,35 +15,67 @@ type BarberShop struct {
 	ClientsChan chan string
 }
 
+// func (shop *BarberShop) AddBarber(barber string) {
+// 	shop.TotalBarbers++
+
+// 	go func() {
+// 		isSleeping := false
+
+// 		color.Yellow("%s is checking the waiting room" , barber)
+
+// 		for {
+// 			if len(shop.ClientsChan) == 0 {
+// 				isSleeping = true
+// 				color.Red("%s is going to sleep" , barber)
+// 			}
+// 			//is shop is close we will close the channels and shopOpen will become false
+// 			client , shopOpen := <-shop.ClientsChan
+// 			if(shopOpen) {
+// 				if isSleeping {
+// 					color.Yellow("%s wakes %s to cut his hair" , client , barber)
+// 					isSleeping = false
+// 				}
+
+// 				shop.CutHair(barber , client)
+
+// 			} else {
+// 				shop.SendBarberHome(barber)
+// 				return
+// 			}
+// 		}
+
+// 	}()
+// }
+
 func (shop *BarberShop) AddBarber(barber string) {
 	shop.TotalBarbers++
 
 	go func() {
 		isSleeping := false
-
-		color.Yellow("%s is checking the waiting room" , barber)
+		color.Yellow("%s goes to the waiting room to check for clients.", barber)
 
 		for {
+			// if there are no clients, the barber goes to sleep
 			if len(shop.ClientsChan) == 0 {
+				color.Yellow("There is nothing to do, so %s takes a nap.", barber)
 				isSleeping = true
-				color.Red("%s is going to sleep" , barber)
 			}
-			//is shop is close we will close the channels and shopOpen will become false
-			client , shopOpen := <-shop.ClientsChan
-			if(shopOpen) {
+
+			client, shopOpen := <-shop.ClientsChan
+
+			if shopOpen {
 				if isSleeping {
-					color.Yellow("%s wakes %s to cut his hair" , client , barber)
+					color.Yellow("%s wakes %s up.", client, barber)
 					isSleeping = false
 				}
-
-				shop.CutHair(barber , client)
-
+				// cut hair
+				shop.CutHair(barber, client)
 			} else {
+				// shop is closed, so send the barber home and close this goroutine
 				shop.SendBarberHome(barber)
 				return
 			}
 		}
-
 	}()
 }
 
@@ -56,4 +88,32 @@ func (shop *BarberShop) CutHair(barber , client string) {
 func (shop *BarberShop) SendBarberHome(barber string) {
 	color.Cyan("%s is going home" , barber)
 	shop.BarbersDoneChan <- true
+}
+
+func (shop *BarberShop) ShopClosingForDay() {
+	color.Cyan("Shop Closing")
+	close(shop.ClientsChan)
+	shop.Open = false
+
+	for i := 0; i < shop.TotalBarbers; i++ { //block till every barber is done
+		<-shop.BarbersDoneChan
+	}
+
+	close(shop.BarbersDoneChan)
+	color.Cyan("Shop Closed. Barbers done for the day.")
+}
+
+func (shop *BarberShop) AddClient(client string) {
+	color.Green("*** %s arrives!", client)
+
+	if shop.Open {
+		select {
+		case shop.ClientsChan <- client:
+			color.Yellow("%s takes a seat in the waiting room.", client)
+		default:
+			color.Red("The waiting room is full, so %s leaves.", client)
+		}
+	} else {
+		color.Red("The shop is already closed, so %s leaves!", client)
+	}
 }
